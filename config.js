0475074c0d5fec0053587c29387178901651bae8
Z̟̈́̆̉͜C̵͉͋̔͞Ḛͭ̉̇͟N̺̻̔̆ͅἿƉ <85298695+madeganz@users.noreply.github.com>
2021-11-18 18:25:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['687965044', '687965044'] // Letakan nomor kamu disini
+global.owner = ['687965044', '994402245101'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
